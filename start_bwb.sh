21539da5c939e42eaad92de165f8d8321283b309
Thomas Dahlstrom <tdahlstrom@gmail.com>
2023-08-01 16:12:07 -0700

    Start Bwb with docker script added.

---
+++ start_bwb.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -p 6080:6080 -v /workspace/gitpod-workflow-test/:/data -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add root biodepot/bwb
